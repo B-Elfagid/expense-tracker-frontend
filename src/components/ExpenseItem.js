9219d5826c4e76b17c488781e068fe99632c82a8
Betty <betty_elfagid@yahoo.co.uk>
2022-01-18 16:18:17 +0000

    expenseitem and expenseitem.css added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+     <div className="expense-item">
+       <div>January 18th 2022</div>
+       <div className="expense-item__description">
+         <h2>Car Insurance</h2><div className="expense-item__price">£299</div>
+       </div>
+     </div>
+    )
+}
+
+export default ExpenseItem;
